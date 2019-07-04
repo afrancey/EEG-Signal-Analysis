cad921e3cda0f1cfd8eb878a84f84d8ca111e0ad
afrancey <adam.francey.af@gmail.com>
2019-07-04 18:53:13 -0400

    removing unwanted code

--- processAll.py
+++ processAll.py
@@ -16,62 +16,9 @@ ang_freqs = 2*np.pi*np.linspace(float(Fs)/nout, Fs, nout)
 ang_freqs = ang_freqs[0:int(len(ang_freqs)/4.)]
 
 pathToOrganized = 'D:\\'
-#pathToOrganized = "C:\\Users\\alzfranc\\Desktop\\"
 
 outputpath = pathToOrganized + 'ORGANIZED\\PERIODOGRAMS_TEST_FROMFILE\\'
 
-
-# forward
-#forig = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\FORWARD\\EEG\\exported - filtered\\'
-#freject = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\FORWARD\\EEG\\exported - post rejections\\'
-#fnotes = 'notes'
-
-#forig = pathToOrganized + 'ORGANIZED\\data\\FORWARD\\EEG\\exported - filtered\\'
-#freject = pathToOrganized + 'ORGANIZED\\data\\FORWARD\\EEG\\exported - post rejections\\'
-#fnotes = 'notes'
-
-forig = pathToOrganized + 'ORGANIZED\\data\\TEST\\exported - filtered\\'
-freject = pathToOrganized + 'ORGANIZED\\data\\TEST\\exported - post rejections\\'
-fnotes = 'notes'
-
-# backward
-#borig = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\BACKWARD\\EEG\\exported - filtered\\'
-#breject = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\BACKWARD\\EEG\\exported - post rejections\\'
-#bnotes = 'notes'
-
-borig = pathToOrganized + 'ORGANIZED\\data\\BACKWARD\\EEG\\exported - filtered\\'
-breject = pathToOrganized + 'ORGANIZED\\data\\BACKWARD\\EEG\\exported - post rejections\\'
-bnotes = 'notes'
-
-# new york
-#norig = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\NEWYORK\\EEG\\exported - filtered\\'
-#nreject = 'C:\\Users\\Adam Francey\\Desktop\\ORGANIZED\\data\\NEWYORK\\EEG\\exported - post rejections\\'
-#nnotes = 'notes'
-
-norig = pathToOrganized + 'ORGANIZED\\data\\NEWYORK\\EEG\\exported - filtered\\'
-nreject = pathToOrganized + 'ORGANIZED\\data\\NEWYORK\\EEG\\exported - post rejections\\'
-nnotes = 'notes'
-
-# custom
-corig = pathToOrganized + 'ORGANIZED\\data\\TEST_FROMFILE\\exported - filtered\\'
-creject = pathToOrganized + 'ORGANIZED\\data\\TEST_FROMFILE\\exported - post rejections\\'
-cnotes = 'notes'
-
-# dictionary of all filepaths
-filepaths = {'forward':{'orig': forig, 'reject' : freject, 'notes':fnotes},
-             'backward':{'orig': borig, 'reject' : breject, 'notes':bnotes},
-             'newyork': {'orig': norig, 'reject' : nreject, 'notes':nnotes},
-             'custom': {'orig': corig, 'reject' : creject, 'notes':cnotes},
-            }
-
-
-def getListOfIDs(filenames):
-
-    IDs = []
-    for name in filenames:
-        IDs.append(name[:name.rindex('-')]) # everything before last (second) dash
-        
-    return set(IDs)
 
 
 # forward to test
@@ -135,34 +82,7 @@ for ID in IDs:
         else:
             print "ERROR: " + eset.error
             errorCount+=1
-
-
-
-        # legacy after setCount +=1 --------
-            
-##        if eset.indicatorArray == False:
-##            bugCount += 1
-##            print "PERCENT AFFECTED: " + str(100*bugCount/setCount)
-##            stringToWrite += orig + "\n"
-##        else:
-##            if eset.TEST_indicator(eset.originalSet,eset.rejectSet, eset.indicatorArray) and eset.rejectSet != "file does not exist":
-##                try:
-##                    pgrams, bandpowers, relative = eset.process(freqs)
-##                    with open(outputpath + UUID + "-" + stop + ".txt",'w') as f:
-##
-##                        for sample in range(len(pgrams[0])):
-##                            f.write(str(pgrams[0][sample]) + '\t' + str(pgrams[1][sample]) + '\t'
-##                                    + str(pgrams[2][sample]) + '\t' + str(pgrams[3][sample]) + '\n')
-##                                
-##                                
-##                except ZeroDivisionError:
-##                    print "ZERO DIVISION ERROR"
-##                    zeroDivision+=1
-
-        # legacy-------
                     
-
-    
     
 
 print IDs
