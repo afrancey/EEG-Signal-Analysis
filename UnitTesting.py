c8c6946f29a089cac4e804aeb0a26921e0975a52
afrancey <adam.francey.af@gmail.com>
2019-11-26 18:46:52 -0500

    making EDA testing files

--- UnitTesting.py
+++ UnitTesting.py
@@ -68,21 +68,22 @@ for filename in os.listdir(inputpath):
 
 # TEST EDA
 # make participant file
-with open("C:/Users/alzfr/Desktop/testLombscargle/UnitTesting files/filtered files/EEGsine,sine.txt", "w") as f:
-    f.write("index\ttp9\ttp10\tfp1\tfp2\n")
-    stringtowrite = ""
-    for i in range(len(time_points)):
-        stringtowrite+=str(i) + "\t" + str(data_points[i]) + "\t" + str(data_points[i]) + "\t" + str(data_points[i]) + "\t" + str(data_points[i]) + "\n"
+with open("C:/Users/alzfr/Desktop/testEDA/UnitTesting files/raw files/const10.txt", "w") as f:
+    f.write("1524483828.23397\n4.0000")
+    values = [10 for x in range(1000)]
+    stringToWrite = ""
+    for v in range(values):
+        stringtowrite+="\n" + v
     f.write(stringtowrite)
 
 
-boundaries = ["EEGsine,sine.txt,0 8800 ,0 4400 8800 13200 ,4400 13200 ,400 600 2746 3422 7882 8999 9032 10000 "]
+boundaries = ["const10.txt,0 100 200 250 333 448 882 932 "]
 # make boundaries file
-with open("C:/Users/alzfr/Desktop/testLombscargle/UnitTesting files/boundstest.csv", "w") as f:
+with open("C:/Users/alzfr/Desktop/testEDA/UnitTesting files/boundstest.csv", "w") as f:
     for i in boundaries:
         f.write(i + "\n")
 
-inputpath = "C:/Users/alzfr/Desktop/testEDA/UnitTesting files/filtered files/" # folder which contains EEG files
+inputpath = "C:/Users/alzfr/Desktop/testEDA/UnitTesting files/raw files/" # folder which contains EEG files
 boundaryfilepath = "C:/Users/alzfr/Desktop/testEDA/UnitTesting files/boundstest.csv" # path to boundaries
 outputfilepath = "C:/Users/alzfr/Desktop/testEDA/UnitTesting files/output.csv"
 
@@ -91,22 +92,22 @@ stringToWrite = ""
 import time
 startTime = time.time()
 
-for filename in os.listdir(inputpath):
-    if "EDA" in filename:
-
-        print("Calculating periodograms for file: " + filename)
-        eset = EEGSet(inputpath + filename, boundaryfilepath)
-        pgrams, bandpowers, relative = eset.process()
-
-        # Test output
-        print("Band Order: delta, theta, alpha, beta")
-        print("EEGsine")
-        print("Channel 1 Bandpowers: Expect to see large beta")
-        print(bandpowers[0])
-        print("Channel 2: Expect to see large alpha")
-        print(bandpowers[1])
-        print("Channel 3: Expect to see large theta")
-        print(bandpowers[2])
-        print("Channel 4: Expect to see a mix")
-        print(bandpowers[3])
+##for filename in os.listdir(inputpath):
+##    if "EDA" in filename:
+##
+##        print("Calculating periodograms for file: " + filename)
+##        eset = EEGSet(inputpath + filename, boundaryfilepath)
+##        pgrams, bandpowers, relative = eset.process()
+##
+##        # Test output
+##        print("Band Order: delta, theta, alpha, beta")
+##        print("EEGsine")
+##        print("Channel 1 Bandpowers: Expect to see large beta")
+##        print(bandpowers[0])
+##        print("Channel 2: Expect to see large alpha")
+##        print(bandpowers[1])
+##        print("Channel 3: Expect to see large theta")
+##        print(bandpowers[2])
+##        print("Channel 4: Expect to see a mix")
+##        print(bandpowers[3])
 
